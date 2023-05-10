08e5399119d9f04dc9920ae936b77d5b23caae60
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-10 14:22:22 -0700

    replace forEach loop with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function onPageLoad() {
   let ulElement = document.createElement("ul");
   flavors = ["chocolate", "cookies and cream", "mint chocolate chip"];
-  flavors.forEach(function(flavor) {
+  for (let index = 0; index < flavors.length; index++) {
     let liElement = document.createElement("li");
-    liElement.append(flavor);
+    liElement.append(flavors[index]);
     ulElement.append(liElement);
-  });
+  };
   document.querySelector("body").append(ulElement);
 }
 
